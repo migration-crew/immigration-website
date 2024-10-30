366c003f5a0f172221d026cd6b6ded093ce38e9d
mika.urakawa <awakaru0122@gmail.com>
2024-10-30 12:44:37 -0700

    feat: make a logo button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
+import logo from '../../public/logo.png'
+import Link from 'next/link'
+
 export default function Header() {
     return (
         <header>
-            <p>Header</p>
+            <Link href="/">
+                <Image
+                    src={logo}
+                    alt="logo image"
+                    quality={100}
+                    priority
+                    className="w-40 lg:w-48"
+                />
+            </Link>
         </header>
     )
 }
