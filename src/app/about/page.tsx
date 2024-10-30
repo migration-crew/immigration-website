bcec1ad339c081cf4af2edc4854a5a2d0cbf8677
yuki <yukihiguchi0826@gmail.com>
2024-10-30 01:07:52 -0700

    implement HeaderPicture component and About page for check if it's working

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,18 @@
 import { useTranslations } from "next-intl";
 
+import { HeaderPicture } from "@/components/common/HeaderPicture";
+import about from "@/public/about.jpeg"
+
 export default function AboutPage() {
     const t = useTranslations("products");
 
-    return (
-        <>
-            <div>{t("cart")}</div>
-        </>
+    // return (
+    //     <>
+    //         <div>{t("cart")}</div>
+    //     </>
+    // )
+
+    return(
+        <HeaderPicture alt="about us" src={about} title={t("cart")} />
     )
 }
--- src/components/common/HeaderPicture.tsx
+++ src/components/common/HeaderPicture.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Image, { StaticImageData } from "next/image";
 
-export const headerPicture = () => {
+type Props = {
+  alt: string;
+  src: StaticImageData;
+  title: string;
+};
+
+export const HeaderPicture = ({ alt, src, title }: Props) => {
   return (
-    <div>headerPicture</div>
-  )
-}
+    <section className="relative min-h-[508px] md:min-h-[624px]">
+      <Image alt={alt} src={src} layout={"fill"} objectFit={"cover"} />
+      <div className="absolute w-full h-full grid place-content-center bg-gray-400 bg-opacity-50">
+        <h1 className="text-[36px] md:text-[48px] font-bold text-primary-white">
+          {title}
+        </h1>
+      </div>
+    </section>
+  );
+};
