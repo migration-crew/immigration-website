aea7755638cbf68449910b3824c59c46267903e2
mika.urakawa <awakaru0122@gmail.com>
2024-10-30 16:08:11 -0700

    feat: navigation menu for desktop

---
+++ src/components/common/header/_components/UpImmigrationNavigationMenu.tsx
@@ -0,0 +1,85 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+import { useTranslations } from "next-intl"
+import { HeaderComponent } from "../Header"
+
+type Props = {
+    immigrateComponents: HeaderComponent[]
+    workComponents: HeaderComponent[]
+}
+
+export default function UpImmigrationNavigationMenu({immigrateComponents, workComponents}: Props) {
+    const t = useTranslations("NavigationMenu")
+
+    return (
+        <NavigationMenu>
+            <NavigationMenuList className='space-x-6'>
+                {/* Immigrate */}
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>{t("immigrate")}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="w-[300px] lg:w-[300px]">
+                            {immigrateComponents.map((component, index) => (
+                                <Link key={index} href={component.href}>
+                                    {component.title}
+                                </Link>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                {/* Work */}
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>{t("work")}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            {workComponents.map((component, index) => (
+                                <Link key={index} href={component.href}>
+                                    {component.title}
+                                </Link>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                {/* Study */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/study">
+                        {t("study")}
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                {/* About us */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/about">
+                        {t("aboutUs")}
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                {/* Contact */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/contact">
+                        {t("contact")}
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                {/* Blog */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/blog">
+                        {t("blog")}
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                {/* FAQ */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/faq">
+                        {t("faq")}
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
