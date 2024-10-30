9538f4652ca9976e0a035a0f78bd1bdf454f820c
yuki <yukihiguchi0826@gmail.com>
2024-10-30 11:19:37 -0700

    remove useState

--- src/components/common/faq.tsx
+++ src/components/common/faq.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   Accordion,
   AccordionContent,
@@ -8,38 +6,22 @@ import {
 } from "@/components/ui/FAQaccordion";
 import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { QAItem } from "@/data/FAQData";
-import { useState } from "react";
 
 type Props = {
-  data: QAItem[]
-}
+  data: QAItem[];
+};
 
-const FAQ = ({data}:Props) => {
-  const [openItems, setOpenItems] = useState<string[]>([]);
-
-  const toggleItem = (itemId: string) => {
-    setOpenItems((prev) =>
-      prev.includes(itemId)
-        ? prev.filter((id) => id !== itemId)
-        : [...prev, itemId]
-    );
-  };
+const FAQ = ({ data }: Props) => {
   return (
     <section className="container mx-auto py-8">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">FAQ</CardTitle>
       </CardHeader>
       <CardContent>
-        <Accordion
-          type="multiple"
-          value={openItems}
-          onValueChange={setOpenItems}
-        >
+        <Accordion type="multiple">
           {data.map((item) => (
             <AccordionItem key={item.id} value={item.id}>
-              <AccordionTrigger onClick={() => toggleItem(item.id)}>
-                {item.question}
-              </AccordionTrigger>
+              <AccordionTrigger>{item.question}</AccordionTrigger>
               <AccordionContent>{item.answer}</AccordionContent>
             </AccordionItem>
           ))}
