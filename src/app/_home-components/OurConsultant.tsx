1449bdede3fa4b0f66c829292f86c28289a17ed5
yuki <yukihiguchi0826@gmail.com>
2024-10-30 23:12:17 -0700

    implement OurConsultant component

---
+++ src/app/_home-components/OurConsultant.tsx
@@ -0,0 +1,57 @@
+import { SectionContainer } from "@/components/common/SectionContainer";
+import CICCLogo from "@/public/CICCLogo.png";
+import Larissa from "@/public/Larissa.png";
+import Image from "next/image";
+
+export const OurConsultant = () => {
+  return (
+    <SectionContainer className="pt-[100px] pb-[50px] lg:flex">
+      <div className="max-w-[640px]">
+        <h2 className="text-heading-mobile lg:text-heading">Our consultant</h2>
+        <h3 className="text-subheading-mobile lg:text-subheading py-4 lg:pb-0 ">
+          Larissa Castelluber
+        </h3>
+        <div className="relative w-[398px] h-[316px] lg:hidden">
+          <Image
+            alt="Larissa"
+            src={Larissa}
+            layout={"fill"}
+            objectFit={"cover"}
+            className="rounded-3xl"
+          />
+        </div>
+        <p className="text-paragraph-text-mobile lg:text-paragraph-text py-4 pb-8 lg:py-8">
+          I&apos;m committed to helping you realize your Canadian dream. I
+          understand the complexities of the immigration process and am here to
+          offer the support and guidance you need. Together, we can ensure your
+          journey to Canada is successful and fulfilling. Proudly regulated and
+          in good standing with the College of Immigration and Citizenship
+          Consultants, Larissa Castelluber, registration R710678
+        </p>
+        <div className="flex justify-between items-end">
+          <div>
+            <p>Registration: R710678</p>
+            <p className="text-[#E92736]">Verify with ICCRC</p>
+          </div>
+          <div className="relative w-[185px] h-[45px] lg:w-[309px] lg:h-[75px]">
+            <Image
+              alt="CICCLogo"
+              src={CICCLogo}
+              layout={"fill"}
+              objectFit={"cover"}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="relative w-[398px] h-[316px] ml-[42px] hidden lg:block">
+        <Image
+          alt="Larissa"
+          src={Larissa}
+          layout={"fill"}
+          objectFit={"cover"}
+          className="rounded-3xl"
+        />
+      </div>
+    </SectionContainer>
+  );
+};
