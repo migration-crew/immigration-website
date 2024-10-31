098ad735f605e4137ecb0d4f8310f950de9afc0f
yuki <yukihiguchi0826@gmail.com>
2024-10-30 17:08:52 -0700

    make about us for test design

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,49 @@
 import { useTranslations } from "next-intl";
 
 import { HeaderPicture } from "@/components/common/HeaderPicture";
-import about from "@/public/about.jpeg"
+import { SectionContainer } from "@/components/common/SectionContainer";
+import about from "@/public/about.jpeg";
 
 export default function AboutPage() {
-    const t = useTranslations("products");
+  const t = useTranslations("products");
 
-    // return (
-    //     <>
-    //         <div>{t("cart")}</div>
-    //     </>
-    // )
+  // return (
+  //     <>
+  //         <div>{t("cart")}</div>
+  //     </>
+  // )
 
-    return(
-        <HeaderPicture alt="about us" src={about} title={t("cart")} />
-    )
+  return (
+    <>
+      <HeaderPicture alt="about us" src={about} title={t("cart")} />
+      <SectionContainer>
+        <h1>Who we are?</h1>
+        <p>
+          Here at Up Immigration, we are dedicated to making your Canadian dream
+          a reality. Our passion lies in helping individuals and families
+          navigate the complexities of immigration with confidence.
+        </p>
+      </SectionContainer>
+      <SectionContainer>
+        <div>picture section</div>
+        <div>
+          <h3>Larissa Castelluber</h3>
+          <p>
+            Meet Larissa, a highly qualified and dedicated Regulated Canadian
+            Immigration Consultant (RCIC). Larissa is a member of the
+            Immigration Consultants of Canada Regulatory Council (ICCRC) and the
+            Canadian Association of Professional Immigration Consultants
+            (CAPIC).
+          </p>
+          <p>
+            Larissa brings with her a solid educational background to her
+            practice. She holds a certificate in Canadian Immigration Law,
+            Policy, and Procedures from the University of British Columbia
+            (UBC), making her well-versed in the legal and regulatory aspects of
+            the Canadian immigration process.
+          </p>
+        </div>
+      </SectionContainer>
+    </>
+  );
 }
