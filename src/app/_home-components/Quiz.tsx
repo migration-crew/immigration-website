9006d8fb3bfd6d5ecfdfd65d928fc3054ab59d8d
yuki <yukihiguchi0826@gmail.com>
2024-10-31 00:25:29 -0700

    implement Quiz component

---
+++ src/app/_home-components/Quiz.tsx
@@ -0,0 +1,11 @@
+import { SectionContainer } from '@/components/common/SectionContainer'
+import React from 'react'
+
+export const Quiz = () => {
+  return (
+    <SectionContainer>
+      <h1 className='pb-4 text-heading-mobile lg:text-heading'>Take the Quiz to find your immigration pathway!</h1>
+      <p>By taking this quiz, you will discover which immigration pathway best fits your unique situation.</p>
+    </SectionContainer>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { CanadianImmigrationServices } from "./_home-components/CanadianImmigrationServices";
 import { OurConsultant } from "./_home-components/OurConsultant";
+import { Quiz } from "./_home-components/Quiz";
 import { Title } from "./_home-components/Title";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Title />
     <OurConsultant />
     <CanadianImmigrationServices />
+    <Quiz />
     </>
   )
 }
