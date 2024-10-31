5a46e58484216d39e0deed7d02dc59e3ec03b4a3
yuki <yukihiguchi0826@gmail.com>
2024-10-31 12:28:11 -0700

    change the height of HEaderPicture

--- src/components/common/HeaderPicture.tsx
+++ src/components/common/HeaderPicture.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const HeaderPicture = ({ alt, src, title }: Props) => {
   return (
-    <section className="relative min-h-[508px] md:min-h-[624px]">
+    <section className="relative min-h-[218px] md:min-h-[360px]">
       <Image alt={alt} src={src} layout={"fill"} objectFit={"cover"} quality={100} />
       <div className="absolute w-full h-full grid place-content-center bg-secondary-blue-op-20">
         <h1 className="text-[36px] md:text-[48px] font-bold text-primary-white">
