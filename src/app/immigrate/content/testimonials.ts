92f76c5eb02fe81d37ee9cf080fd0b6dc4127e6b
mika.urakawa <awakaru0122@gmail.com>
2024-11-01 11:56:12 -0700

    fix: fix image issue in testimonial

--- src/app/immigrate/content/testimonials.ts
+++ src/app/immigrate/content/testimonials.ts
@@ -1,20 +1,30 @@
-export const Testimonials = [
+import larissa from "@/public/Larissa.png";
+import { StaticImageData } from "next/image";
+
+type TestimonialsType = {
+  name: string
+  country: string
+  text: string
+  image: StaticImageData
+}
+
+export const Testimonials: TestimonialsType[] = [
   {
     name: "Laura Ribeiro",
     country: "Brazil",
     text: "I had an amazing experience with my visa application. They made the entire process easy and stress-free. The staff's expertise and attention to detail were impressive.",
-    image: "/placeholder.svg?height=100&width=100",
+    image: larissa,
   },
   {
     name: "Maria Torres",
     country: "Mexico",
     text: "I had an amazing experience with my visa application. They made the entire process easy and stress-free. The staff's expertise and attention to detail were impressive.",
-    image: "/placeholder.svg?height=100&width=100",
+    image: larissa,
   },
   {
     name: "Juan Sanchez",
     country: "Colombia",
     text: "I had an amazing experience with my visa application. They made the entire process easy and stress-free. The staff's expertise and attention to detail were impressive.",
-    image: "/placeholder.svg?height=100&width=100",
+    image: larissa,
   },
 ];
