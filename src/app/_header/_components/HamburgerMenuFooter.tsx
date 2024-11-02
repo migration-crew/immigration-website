896aebfb47d0edae6d785fed4fb3a42be775fc5c
mika.urakawa <awakaru0122@gmail.com>
2024-11-02 00:01:32 -0700

    fix: delete language switcher for now
    because when a user clicks select box, the sheet will be closed. then cannot choose a language

--- src/app/_header/_components/HamburgerMenuFooter.tsx
+++ src/app/_header/_components/HamburgerMenuFooter.tsx
@@ -11,18 +11,12 @@ export default function HamburgerMenuFooter({ components }: Props) {
     const t = useTranslations("LocaleSwitcher")
 
     return (
-        <div className="fixed bottom-10">
-            <div className="flex items-center justify-between mb-5 gap-x-4">
-                <OtherButton>{t("language")}</OtherButton>
-                <LanguageSwitcher />
-            </div>
-            <div className="flex space-x-4">
-                {components.map((component) => (
-                    <a target="_blank" key={component.name} href={component.href}>
-                        <component.imageComponent size={35} />
-                    </a>
-                ))}
-            </div>
+        <div className="fixed bottom-10 flex space-x-4">
+            {components.map((component) => (
+                <a target="_blank" key={component.name} href={component.href}>
+                    <component.imageComponent size={35} />
+                </a>
+            ))}
         </div>
     )
 }
