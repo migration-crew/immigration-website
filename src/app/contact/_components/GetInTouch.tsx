5b1252d8c9c53bda5a98b9ec2fe6d2bc23931d0d
mika.urakawa <awakaru0122@gmail.com>
2024-11-02 19:10:04 -0700

    fix: fix get in touch

--- src/app/contact/_components/GetInTouch.tsx
+++ src/app/contact/_components/GetInTouch.tsx
@@ -7,20 +7,21 @@ import { Card } from "@/components/ui/card"
 
 export function GetInTouch() {
   return (
-    <Card className="w-[446px] p-6">
-      <Heading className="mb-7">Get in touch</Heading>
+    <Card className="min-w-[300px] min-h-[280px] p-6 flex flex-col h-full justify-between">
+      <div>
+        <Heading className="mb-7">Get in touch</Heading>
         <div className="space-y-3">
           <div className="flex space-x-3 items-center">
-            <Whatsapp size={25}/>
+            <Whatsapp size={25} />
             <OtherButton>+1 672-588-1360</OtherButton>
           </div>
           <div className="flex space-x-3 items-center">
-            <Mail size={25}/>
+            <Mail size={25} />
             <OtherButton>info@upimmigration.ca</OtherButton>
           </div>
         </div>
-      {/* Wanna add fixed bottom-10 to this component after merging my PR */}
-      <BookFreeConsultation/>
+      </div>
+      <BookFreeConsultation className="mt-auto w-full" />
     </Card>
   );
 }
