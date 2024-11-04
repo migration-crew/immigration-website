2bb4abc9b01fac671f5f3acb3e9ce746c8fc4495
yuki <yukihiguchi0826@gmail.com>
2024-11-03 22:56:52 -0800

    implement quiz

--- src/app/_home-components/Quiz.tsx
+++ src/app/_home-components/Quiz.tsx
@@ -1,23 +1,35 @@
-import { ResponsiveImage } from '@/components/common/ResponsiveImage'
-import { SectionContainer } from '@/components/common/SectionContainer'
-import { Heading } from '@/components/common/text/Heading'
-import { Card, CardFooter, CardHeader } from '@/components/ui/card'
-import startQuiz from "@/public/startQuiz.png"
-import React from 'react'
+import { ResponsiveImage } from "@/components/common/ResponsiveImage";
+import { SectionContainer } from "@/components/common/SectionContainer";
+import { Heading } from "@/components/common/text/Heading";
+import { Title } from "@/components/common/text/Title";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import startQuiz from "@/public/startQuiz.png";
+
 
 export const Quiz = () => {
   return (
-    <SectionContainer>
-      <Heading className='pb-4'>Take the Quiz to find your immigration pathway!</Heading>
+    <SectionContainer className="py-10 xl:py-[50px]">
+      <Heading>
+        Take the Quiz to find your immigration pathway!
+      </Heading>
 
-      <p>By taking this quiz, you will discover which immigration pathway best fits your unique situation.</p>
+      <p className="py-4">
+        By taking this quiz, you will discover which immigration pathway best
+        fits your unique situation.
+      </p>
 
-      <Card className='h-[602px]'>
-        <CardHeader className='bg-secondary-blue rounded-t-xl h-[474px] mb-0'>
-          <ResponsiveImage alt='startQuiz' src={startQuiz} divClassName='h-[400px] w-[500px]' imgClassName=''></ResponsiveImage>
+      <Card className="flex flex-col h-[398px] xl:h-[602px]">
+        <CardHeader className="bg-secondary-blue-op-20 rounded-t-xl h-[306px] xl:h-[474px] pb-0 flex justify-end items-center">
+          <ResponsiveImage
+            alt="startQuiz"
+            src={startQuiz}
+            divClassName="h-[300px] w-[350px] xl:h-[400px] xl:w-[500px]"
+          ></ResponsiveImage>
         </CardHeader>
-        <CardFooter></CardFooter>
+        <CardFooter className="bg-primary-red flex-1 rounded-b-xl grid place-content-center p-0 xl:p-6">
+          <Title className="text-primary-white text-[36px]">Start Quiz </Title>
+        </CardFooter>
       </Card>
     </SectionContainer>
-  )
-}
+  );
+};
