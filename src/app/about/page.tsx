0c55b5366628b1737b305987df1b34b9ff96d14e
yuki <yukihiguchi0826@gmail.com>
2024-11-04 11:29:16 -0800

    implement Who we are first part

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,6 +15,8 @@ import { Paragraph } from "@/components/common/text/Paragraph";
 import { SubHeading } from "@/components/common/text/SubHeading";
 import { Title } from "@/components/common/text/Title";
 import about from "@/public/about.jpeg";
+import { ResponsiveImage } from "@/components/common/ResponsiveImage";
+import Larissa from "@/public/Larissa.png"
 
 export default function AboutPage() {
   const t = useTranslations("products");
@@ -27,35 +29,35 @@ export default function AboutPage() {
 
   return (
     <>
-      <HeaderPicture alt="about us" src={about} title={t("cart")} />
-      <SectionContainer>
-        <h1>Who we are?</h1>
-        <p>
+      <HeaderPicture alt="about us" src={about} title={"About us"} />
+      <SectionContainer className="py-[100px]">
+        <Heading className="pb-4">Who we are?</Heading>
+        <Paragraph>
           Here at Up Immigration, we are dedicated to making your Canadian dream
           a reality. Our passion lies in helping individuals and families
           navigate the complexities of immigration with confidence.
-        </p>
-      </SectionContainer>
-      <SectionContainer className="xl:flex py-[50px] xl:py-[40px]">
-        <div className="w-1/2">picture section</div>
-        <div className="w-1/2">
-          <h3>Larissa Castelluber</h3>
-          <p>
-            Meet Larissa, a highly qualified and dedicated Regulated Canadian
-            Immigration Consultant (RCIC). Larissa is a member of the
-            Immigration Consultants of Canada Regulatory Council (ICCRC) and the
-            Canadian Association of Professional Immigration Consultants
-            (CAPIC).
-          </p>
-          <p>
-            Larissa brings with her a solid educational background to her
-            practice. She holds a certificate in Canadian Immigration Law,
-            Policy, and Procedures from the University of British Columbia
-            (UBC), making her well-versed in the legal and regulatory aspects of
-            the Canadian immigration process.
-          </p>
+        </Paragraph>
+        <div className="xl:flex xl:gap-6 py-6 xl:pt-[100px] xl:pb-4">
+          <ResponsiveImage alt="Larissa" src={Larissa} divClassName="h-[316px] mb-4 xl:m-0 xl:h-auto xl:w-1/2" imgClassName="rounded-3xl" />
+          <div className="xl:w-1/2">
+            <CtaButton>Larissa Castelluber</CtaButton>
+            <Paragraph>
+              Meet Larissa, a highly qualified and dedicated Regulated Canadian
+              Immigration Consultant (RCIC). Larissa is a member of the
+              Immigration Consultants of Canada Regulatory Council (ICCRC) and
+              the Canadian Association of Professional Immigration Consultants
+              (CAPIC).<br/>
+              <br />
+              Larissa brings with her a solid educational background to her
+              practice. She holds a certificate in Canadian Immigration Law,
+              Policy, and Procedures from the University of British Columbia
+              (UBC), making her well-versed in the legal and regulatory aspects
+              of the Canadian immigration process.
+            </Paragraph>
+          </div>
         </div>
       </SectionContainer>
+
       <SectionContainer>
         <Hero>Hero</Hero>
         <Title>Title</Title>
