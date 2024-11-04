ec99f4fa0e66ea6a5421c1667e84a913102f75d7
yuki <yukihiguchi0826@gmail.com>
2024-11-03 23:22:22 -0800

    add title, className, bg-color to faq component

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,10 +1,14 @@
-import FAQ from "@/components/common/faq"
-import { qaGeneralData } from "@/data/FAQData"
+import FAQ from "@/components/common/faq";
+import { qaGeneralData } from "@/data/FAQData";
 
 const FAQPage = () => {
-  return(
-    <FAQ data={qaGeneralData} />
-  )
-}
+  return (
+    <FAQ
+      data={qaGeneralData}
+      title="FAQ"
+      className="py-8"
+    />
+  );
+};
 
-export default FAQPage
+export default FAQPage;
--- src/components/common/faq.tsx
+++ src/components/common/faq.tsx
@@ -6,28 +6,53 @@ import {
 } from "@/components/ui/FAQaccordion";
 import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { QAItem } from "@/data/FAQData";
+import { cn } from "@/lib/utils";
+import { SectionContainer } from "./SectionContainer";
 
 type Props = {
   data: QAItem[];
+  title: string;
+  bgColor?: string | undefined;
+  className?: string | undefined;
 };
 
-const FAQ = ({ data }: Props) => {
+/**
+ * Common Section Container
+ * @param data - pass FAQ data
+ * @param title - pass title
+ * @param bgColor - pass background color
+ * @param className - pass custom classes
+ * @example
+ * ```tsx
+ *  <FAQ
+      data={qaGeneralData}
+      title="FAQ"
+      bgColor="bg-neutral-secondary-white"
+      className="py-8"
+    />
+ * ```
+ */
+const FAQ = ({ data, title, bgColor, className }: Props) => {
   return (
-    <section className="container mx-auto py-8">
+    <SectionContainer bgColor={cn(bgColor)} className={cn(className)}>
       <CardHeader>
-        <CardTitle className="text-heading-mobile xl:text-heading test">FAQ</CardTitle>
+        <CardTitle className="text-heading-mobile xl:text-heading test">
+          {title}
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <Accordion type="multiple">
           {data.map((item, index) => (
             <AccordionItem key={item.id} value={item.id}>
-              <AccordionTrigger>{`${index + 1}. ${item.question}`}</AccordionTrigger>
+              <AccordionTrigger>{`${index + 1}. ${
+                item.question
+              }`}</AccordionTrigger>
               <AccordionContent>{item.answer}</AccordionContent>
             </AccordionItem>
           ))}
         </Accordion>
       </CardContent>
-    </section>
+    </SectionContainer>
   );
 };
 
