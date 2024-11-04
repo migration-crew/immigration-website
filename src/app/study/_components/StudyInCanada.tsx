6acc749bea9929a5fafe3d3f4be1ad1d4ca1673f
David Torrez <dtorrez1996@gmail.com>
2024-11-04 15:04:57 -0500

    adjust font size of welcome message

--- src/app/study/_components/StudyInCanada.tsx
+++ src/app/study/_components/StudyInCanada.tsx
@@ -3,9 +3,9 @@ import { SectionContainer } from "@/components/common/SectionContainer";
 
 export function StudyInCanada() {
   return (
-    <SectionContainer className="flex flex-col gap-4">
+    <SectionContainer className="flex flex-col gap-4 px-4">
       <h2 className="text-xl font-bold ">Why study in Canada?</h2>
-      <p>
+      <p className="text-sm">
         Canada offers world-class education, diverse cultural experiences, and
         potential pathways to permanent residency. International students choose
         Canada for its top-ranked institutions and the opportunity to work
