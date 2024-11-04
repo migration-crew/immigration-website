cbc3dfc85456aaca764adf9343d6fec4d2069ade
YuiKonaka <youthfuldays585@gmail.com>
2024-11-04 14:13:05 -0800

    Fixed Contact Page

--- src/app/contact/_components/ConsultantInfo.tsx
+++ src/app/contact/_components/ConsultantInfo.tsx
@@ -1,21 +1,33 @@
+import { Heading } from "@/components/common/text/Heading";
+import { OtherButton } from "@/components/common/text/OtherButton";
+import { SubHeading } from "@/components/common/text/SubHeading";
+import { Card } from "@/components/ui/card";
 import Consultant from "@/public/Consultant.png";
 import Image from "next/image";
 
 export function ConsultantInfo() {
   return (
     <div className="flex my-14">
-      <Image alt="contact us" src={Consultant} className="" />
+      <Card>
+        <Image
+          alt="contact us"
+          src={Consultant}
+          className="rounded-md"
+          layout="fill"
+        />
+      </Card>
       <div className="ml-4 w-3/4 pt-44">
-        <h1 className="mb-8">How Can We Help You Today?</h1>
-        <h2 className="mb-4">Larissa Castelluber</h2>
-        <p>
-          Hi there,
-          <br />
+        <Heading>How Can We Help You Today?</Heading>
+        <SubHeading className="text-primary-red">
+          Larissa Castelluber
+        </SubHeading>
+        <OtherButton>
+          Hi there, <br />
           I'm Larissa Castelluber, and I'm dedicated to helping you achieve your
           Canadian dream. I understand the challenges of the immigration process
           and am here to provide you with the support and guidance you need.
           Let's make your journey to Canada a successful and fulfilling one!
-        </p>
+        </OtherButton>
       </div>
     </div>
   );
