13184c905895c3c2f975a1be66fd9aae95ff5eb0
yuki <yukihiguchi0826@gmail.com>
2024-11-04 00:17:32 -0800

    implement testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { Testimonial } from "@/components/common/Testimonial";
+import TestimonialMobile from "@/components/common/TestimonialMobile";
+import { testimonialData } from "@/data/testimonials";
 import { CanadianImmigrationServices } from "./_home-components/CanadianImmigrationServices";
 import { OurConsultant } from "./_home-components/OurConsultant";
 import { Quiz } from "./_home-components/Quiz";
@@ -6,10 +9,15 @@ import { Title } from "./_home-components/Title";
 export default function Home() {
   return (
     <>
-    <Title />
-    <OurConsultant />
-    <CanadianImmigrationServices />
-    <Quiz />
+      <Title />
+      <OurConsultant />
+      <CanadianImmigrationServices />
+      <Quiz />
+      <Testimonial
+        testimonials={testimonialData}
+        className="pt-[50px] pb-[100px]"
+      />
+      <TestimonialMobile testimonials={testimonialData} className="py-10" />
     </>
-  )
+  );
 }
