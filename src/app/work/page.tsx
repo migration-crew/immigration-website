23bf8b18cfe7eb90a2ab603f756da981541f618a
David Torrez <dtorrez1996@gmail.com>
2024-11-04 12:33:15 -0500

    Update import in work/page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,10 +3,10 @@ import FAQ from "@/components/common/faq";
 import { HeaderPicture } from "@/components/common/HeaderPicture";
 import { Testimonial } from "@/components/common/Testimonial";
 import TestimonialMobile from "@/components/common/TestimonialMobile";
+import { FAQs } from "@/data/FAQData";
 import { testimonialData } from "@/data/testimonials";
 import workImage from "@/public/work.jpeg";
 import { StepsComponent } from "../immigrate/_components/Steps";
-import { FAQs } from "../immigrate/content/faqs";
 import { DiscoverOptions } from "./_components/DiscoverOptions";
 import { WorkInCanada } from "./_components/WorkInCanada";
 
