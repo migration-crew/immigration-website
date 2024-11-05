0a138651534a019694dc6c1b72560a7a3ff8d73a
yuki <yukihiguchi0826@gmail.com>
2024-11-04 16:32:56 -0800

    implement WhoWeAre section

---
+++ src/app/about/_components/WhoWeAre.tsx
@@ -0,0 +1,80 @@
+import { ResponsiveImage } from "@/components/common/ResponsiveImage";
+import { SectionContainer } from "@/components/common/SectionContainer";
+import { Button } from "@/components/ui/UpImmigrationButton";
+import Ami from "@/public/Ami.png";
+import Larissa from "@/public/Larissa.png";
+import Shipman from "@/public/Shipman.png";
+import Link from "next/link";
+import { CtaButton } from "@/components/common/text/CtaButton";
+import { Heading } from "@/components/common/text/Heading";
+import { Paragraph } from "@/components/common/text/Paragraph";
+import { Caption } from "@/components/common/text/Caption";
+import { Menu } from "@/components/common/text/Menu";
+
+export const WhoWeAre = () => {
+  return (
+    <SectionContainer className="py-[100px]">
+      <Heading className="pb-4">Who we are?</Heading>
+      <Paragraph>
+        Here at Up Immigration, we are dedicated to making your Canadian dream a
+        reality. Our passion lies in helping individuals and families navigate
+        the complexities of immigration with confidence.
+      </Paragraph>
+      <div className="xl:flex xl:gap-6 py-6 xl:pt-[100px] xl:pb-4">
+        <ResponsiveImage
+          alt="Larissa"
+          src={Larissa}
+          divClassName="h-[316px] mb-4 xl:m-0 xl:h-auto xl:w-1/2"
+          imgClassName="rounded-3xl"
+        />
+        <div className="xl:w-1/2">
+          <CtaButton>Larissa Castelluber</CtaButton>
+          <Caption className="text-disabled-black">RCIC</Caption>
+          <Paragraph className="mt-5">
+            Meet Larissa, a highly qualified and dedicated Regulated Canadian
+            Immigration Consultant (RCIC). Larissa is a member of the
+            Immigration Consultants of Canada Regulatory Council (ICCRC) and the
+            Canadian Association of Professional Immigration Consultants
+            (CAPIC).
+            <br />
+            <br />
+            Larissa brings with her a solid educational background to her
+            practice. She holds a certificate in Canadian Immigration Law,
+            Policy, and Procedures from the University of British Columbia
+            (UBC), making her well-versed in the legal and regulatory aspects of
+            the Canadian immigration process.
+          </Paragraph>
+        </div>
+      </div>
+      <div className="xl:flex xl:gap-6">
+        <div className="flex xl:w-1/2 gap-4 xl:gap-6">
+        <div className="w-1/2">
+          <ResponsiveImage
+            alt="Shipman"
+            src={Shipman}
+            divClassName="w-full h-[170px]"
+            imgClassName="rounded-3xl"
+          />
+          <Menu>Name</Menu>
+          <Caption>Position</Caption>
+          </div>
+          <div className="w-1/2">
+          <ResponsiveImage
+            alt="Ami"
+            src={Ami}
+            divClassName="w-full h-[170px]"
+            imgClassName="rounded-3xl"
+          />
+          <Menu>Name</Menu>
+          <Caption>Position</Caption>
+          </div>
+        </div>
+        <div className="xl:w-1/2 mt-6 xl:m-0">
+          <Button className="w-full xl:w-fit py-3 px-4">
+            <Link href="/book-consultation">Book a consultation</Link>
+          </Button>
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,12 @@
-import { useTranslations } from "next-intl";
-
 import { HeaderPicture } from "@/components/common/HeaderPicture";
-import { SectionContainer } from "@/components/common/SectionContainer";
-import { Caption } from "@/components/common/text/Caption";
-import { CtaButton } from "@/components/common/text/CtaButton";
-import { Heading } from "@/components/common/text/Heading";
-import { HeavyBody } from "@/components/common/text/HeavyBody";
-import { HeavySubHeader } from "@/components/common/text/HeavySubHeader";
-import { Hero } from "@/components/common/text/Hero";
-import { Input } from "@/components/common/text/Input";
-import { Menu } from "@/components/common/text/Menu";
-import { OtherButton } from "@/components/common/text/OtherButton";
-import { Paragraph } from "@/components/common/text/Paragraph";
-import { SubHeading } from "@/components/common/text/SubHeading";
-import { Title } from "@/components/common/text/Title";
+
+
 import about from "@/public/about.jpeg";
-import { ResponsiveImage } from "@/components/common/ResponsiveImage";
-import Larissa from "@/public/Larissa.png"
+import { WhoWeAre } from "./_components/WhoWeAre";
+
 
 export default function AboutPage() {
-  const t = useTranslations("products");
+  // const t = useTranslations("products");
 
   // return (
   //     <>
@@ -30,48 +17,7 @@ export default function AboutPage() {
   return (
     <>
       <HeaderPicture alt="about us" src={about} title={"About us"} />
-      <SectionContainer className="py-[100px]">
-        <Heading className="pb-4">Who we are?</Heading>
-        <Paragraph>
-          Here at Up Immigration, we are dedicated to making your Canadian dream
-          a reality. Our passion lies in helping individuals and families
-          navigate the complexities of immigration with confidence.
-        </Paragraph>
-        <div className="xl:flex xl:gap-6 py-6 xl:pt-[100px] xl:pb-4">
-          <ResponsiveImage alt="Larissa" src={Larissa} divClassName="h-[316px] mb-4 xl:m-0 xl:h-auto xl:w-1/2" imgClassName="rounded-3xl" />
-          <div className="xl:w-1/2">
-            <CtaButton>Larissa Castelluber</CtaButton>
-            <Paragraph>
-              Meet Larissa, a highly qualified and dedicated Regulated Canadian
-              Immigration Consultant (RCIC). Larissa is a member of the
-              Immigration Consultants of Canada Regulatory Council (ICCRC) and
-              the Canadian Association of Professional Immigration Consultants
-              (CAPIC).<br/>
-              <br />
-              Larissa brings with her a solid educational background to her
-              practice. She holds a certificate in Canadian Immigration Law,
-              Policy, and Procedures from the University of British Columbia
-              (UBC), making her well-versed in the legal and regulatory aspects
-              of the Canadian immigration process.
-            </Paragraph>
-          </div>
-        </div>
-      </SectionContainer>
-
-      <SectionContainer>
-        <Hero>Hero</Hero>
-        <Title>Title</Title>
-        <Heading>Heading</Heading>
-        <SubHeading>Sub Heading</SubHeading>
-        <Paragraph>Paragraph</Paragraph>
-        <Caption>Caption</Caption>
-        <CtaButton>CtaButton</CtaButton>
-        <OtherButton>OtherButton</OtherButton>
-        <Menu>Menu</Menu>
-        <Input>Input</Input>
-        <HeavySubHeader>HeavySubHeader</HeavySubHeader>
-        <HeavyBody>HeavyBody</HeavyBody>
-      </SectionContainer>
+      <WhoWeAre />
     </>
   );
 }
