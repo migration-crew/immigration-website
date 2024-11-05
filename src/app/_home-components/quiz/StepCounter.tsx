e9be9e3c99c8d00385641ed712f202e5ca9e8ed4
mika.urakawa <awakaru0122@gmail.com>
2024-11-04 20:20:33 -0800

    feat: modify step counter of quiz

--- src/app/_home-components/quiz/StepCounter.tsx
+++ src/app/_home-components/quiz/StepCounter.tsx
@@ -5,8 +5,22 @@ type Props = {
     totalSteps: number
 }
 
-export default function StepCounter({ currentStep, totalSteps }: Props) {
+export default function StepCounter({ currentStep, totalSteps }: { currentStep: number, totalSteps: number }) {
     return (
-        <Caption>{`Step ${currentStep} of ${totalSteps}`}</Caption>
-    )
+        <div className="space-y-2">
+            <Caption>Step {currentStep} of {totalSteps}</Caption>
+            <div className="flex space-x-2 justify-between">
+                {[...Array(totalSteps)].map((_, index) => (
+                    <div
+                        key={index}
+                        className={`w-6 h-6 rounded-full flex items-center justify-center ${
+                            index + 1 === currentStep ? 'bg-secondary-blue text-primary-white' : 'bg-gray-200'
+                        }`}
+                    >
+                        {index + 1}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
