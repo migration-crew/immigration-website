f024ab78e8c45087ee7f73dc8be7ecf1d0b7fd2b
yuki <yukihiguchi0826@gmail.com>
2024-11-05 17:45:50 -0800

    change the h tag or p tag to text component

--- src/app/_home-components/CanadianImmigrationServices.tsx
+++ src/app/_home-components/CanadianImmigrationServices.tsx
@@ -1,15 +1,15 @@
 import { SectionContainer } from "@/components/common/SectionContainer";
 import { NavCard } from "./NavCard";
 
+import { Heading } from "@/components/common/text/Heading";
 import immigrate from "@/public/immigrateLink.png";
-import work from "@/public/workLink.png";
 import study from "@/public/studyLink.png";
-
+import work from "@/public/workLink.png";
 
 export const CanadianImmigrationServices = () => {
   return (
     <SectionContainer className="py-[40px] xl:py-[50px]">
-      <h2 className="pb-6 text-heading-mobile xl:text-heading">Canadian immigration services</h2>
+      <Heading className="pb-6">Canadian immigration services</Heading>
       <div className="flex flex-col xl:flex-row gap-4 xl:gap-6">
         <NavCard
           alt="immigrate"
