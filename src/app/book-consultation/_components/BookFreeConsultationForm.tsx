4e52a3db8cfb36e9d7eab1f01af45b8f05fa52b9
David Torrez <dtorrez1996@gmail.com>
2024-11-05 23:29:42 -0500

    Add spanish translation for UpImmigrationForm

--- src/app/book-consultation/_components/BookFreeConsultationForm.tsx
+++ src/app/book-consultation/_components/BookFreeConsultationForm.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { UpImmigrationForm } from "@/components/common/UpImmigrationForm/UpImmigrationForm";
 import { Card } from "@/components/ui/card";
+import { useTranslations } from "next-intl";
 
 export function BookFreeConsultationForm() {
+  const t = useTranslations("Form");
+
   const handleOnSubmit = () => {
     // When we wanna do something when the form is submitted, we can do it here
     console.log("Form submitted");
-  }
+  };
 
   return (
     <Card>
-      <UpImmigrationForm title="Book a free initial consultation!" onSubmitCallback={handleOnSubmit}/>
+      <UpImmigrationForm title={t("title")} onSubmitCallback={handleOnSubmit} />
     </Card>
   );
 }
