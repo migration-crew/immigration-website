ae122e5a99557fb1917395eb9d57322ac290998d
yuki <yukihiguchi0826@gmail.com>
2024-11-06 14:23:34 -0800

    change all ApplicationStepMobile props in the immigrate endpoint

--- src/app/immigrate/atlantic/page.tsx
+++ src/app/immigrate/atlantic/page.tsx
@@ -18,7 +18,7 @@ export default function AtlanticImmigration() {
       <IntroductionAtlantic />
 
       <ApplicationStepDesktop steps={EPSteps} title="What is the process?" />
-      <ApplicationStepMobile steps={EPSteps} title="What is the process?" />
+      <ApplicationStepMobile steps={EPSteps} title="What is the process?" circleSpacing={110} height="h-[130px]" />
 
       <EEPrograms />
     </>
