e0f08e0f0d1932929ea1f98a2e5b069dfc72351c
David Torrez <dtorrez1996@gmail.com>
2024-11-05 22:38:41 -0500

    adjust font size and button width in study page

--- src/app/study/_components/StudyCardItem.tsx
+++ src/app/study/_components/StudyCardItem.tsx
@@ -1,4 +1,6 @@
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Paragraph } from "@/components/common/text/Paragraph";
+import { SubHeading } from "@/components/common/text/SubHeading";
+import { CardHeader } from "@/components/ui/card";
 import Image, { StaticImageData } from "next/image";
 
 interface Props {
@@ -12,7 +14,7 @@ export function StudyCard({ title, image, description, alt }: Props) {
   return (
     <div className="flex flex-col gap-4 self-stretch ">
       <CardHeader className="p-0">
-        <CardTitle className="text-xl font-bold">{title}</CardTitle>
+        <SubHeading className="">{title}</SubHeading>
       </CardHeader>
       <div
         className="w-full h-[230px] sm:h-[300px] lg:h-[400px] rounded-[10px]"
@@ -27,9 +29,9 @@ export function StudyCard({ title, image, description, alt }: Props) {
           className="w-full h-full rounded-[10px] opacity-0"
         />
       </div>
-      <CardContent className="text-sm p-0 overflow-hidden text-ellipsis}">
+      <Paragraph className="text-sm p-0 overflow-hidden text-ellipsis}">
         {description}
-      </CardContent>
+      </Paragraph>
     </div>
   );
 }
