e8c4a6b97a7ba28363ab1af9bd9553617d6a2829
mika.urakawa <awakaru0122@gmail.com>
2024-11-06 09:36:50 -0800

    fix: delete mergin top from the footer

--- src/app/_footer/Footer.tsx
+++ src/app/_footer/Footer.tsx
@@ -4,7 +4,7 @@ import PageLinks from "./_components/PageLinks";
 
 export default function Footer() {
     return (
-        <footer className="p-5 xl:pl-20 xl:pt-14 xl:pr-20 xl:pb-24 bg-primary-red mt-10 xl:mt-24 xl:mt-100 xl:flex justify-between space-y-5">
+        <footer className="p-5 xl:pl-20 xl:pt-14 xl:pr-20 xl:pb-24 bg-primary-red mt-0 xl:mt-0 xl:mt-100 xl:flex justify-between space-y-5">
             <Logo />
             <PageLinks />
             <ContactAndCopyRight />
