63d95147fb4a890a3fb99bfd670a9eae37209ab4
mika.urakawa <awakaru0122@gmail.com>
2024-11-06 12:03:42 -0800

    refactor: make a quiz component

---
+++ src/app/_home-components/quiz/Quiz.tsx
@@ -0,0 +1,53 @@
+import { QuizType } from "./QuizDialog";
+import {
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/QuizDialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import StepCounter from "./StepCounter";
+import AnswerButtons from "./AnswerButtons";
+import QuizPagination from "./QuizPagination";
+import { CtaButton } from "@/components/common/text/CtaButton";
+
+type Props = {
+    quiz: QuizType[];
+    currentStep: number;
+    onClose: () => void;
+    onNext: () => void;
+    onBack: () => void;
+    onGoLast: () => void;
+}
+
+export default function Quiz({ quiz, currentStep, onClose, onNext, onBack, onGoLast }: Props) {
+
+    return (
+        <>
+            <DialogHeader>
+                <DialogTitle className="mt-10">
+                    <button onClick={onClose}>
+                        <Cross2Icon className="fixed right-5 top-5 h-7 w-7" />
+                    </button>
+                    <StepCounter
+                        currentStep={currentStep}
+                        totalSteps={quiz.length}
+                    />
+                </DialogTitle>
+            </DialogHeader>
+            <CtaButton className="mt-5">
+                {quiz[currentStep - 1]?.question}
+            </CtaButton>
+            <AnswerButtons answers={quiz[currentStep - 1]?.answers || []} />
+            <DialogFooter className="sm:justify-start mt-16">
+                <QuizPagination
+                    currentStep={currentStep}
+                    totalSteps={quiz.length}
+                    onNext={onNext}
+                    onBack={onBack}
+                    onGoLast={onGoLast}
+                />
+            </DialogFooter>
+        </>
+    )
+
+}
--- src/app/_home-components/quiz/QuizDialog.tsx
+++ src/app/_home-components/quiz/QuizDialog.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { CtaButton } from "@/components/common/text/CtaButton";
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
 } from "@/components/ui/QuizDialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
-import { useTranslations } from "next-intl";
 import { useState } from "react";
-import AnswerButtons from "./AnswerButtons";
-import QuizPagination from "./QuizPagination";
-import StepCounter from "./StepCounter";
+import Quiz from "./Quiz";
 import QuizResult from "./QuizResult";
 
 export type QuizType = {
@@ -28,8 +20,6 @@ type Props = {
 };
 
 export default function QuizDialog({ quiz, isOpen, onClose }: Props) {
-  const t = useTranslations("Home");
-
   const [currentStep, setCurrentStep] = useState(1);
   const [isOpenResult, setIsOpenResult] = useState(false);
 
@@ -64,34 +54,16 @@ export default function QuizDialog({ quiz, isOpen, onClose }: Props) {
     <Dialog open={isOpen}>
       <DialogContent className="h-[600px] xl:h-[600px]">
         {isOpenResult ? (
-          <QuizResult onClose={handleLastClose}/>
+          <QuizResult onClose={handleLastClose} />
         ) : (
-          <>
-            <DialogHeader>
-              <DialogTitle className="mt-10">
-                <button onClick={handleOnClose}>
-                  <Cross2Icon className="fixed right-5 top-5 h-7 w-7" />
-                </button>
-                <StepCounter
-                  currentStep={currentStep}
-                  totalSteps={quiz.length}
-                />
-              </DialogTitle>
-            </DialogHeader>
-            <CtaButton className="mt-5">
-              {quiz[currentStep - 1]?.question}
-            </CtaButton>
-            <AnswerButtons answers={quiz[currentStep - 1]?.answers || []} />
-            <DialogFooter className="sm:justify-start mt-16">
-              <QuizPagination
-                currentStep={currentStep}
-                totalSteps={quiz.length}
-                onNext={handleNext}
-                onBack={handleBack}
-                onGoLast={handleOnGoLast}
-              />
-            </DialogFooter>
-          </>
+          <Quiz
+            quiz={quiz}
+            currentStep={currentStep}
+            onClose={handleOnClose}
+            onNext={handleNext}
+            onBack={handleBack}
+            onGoLast={handleOnGoLast}
+          />
         )}
       </DialogContent>
     </Dialog>
