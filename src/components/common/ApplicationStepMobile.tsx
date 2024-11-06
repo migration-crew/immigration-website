730266b226adc6c5ea5fd4717a39bd7d05ccc715
yuki <yukihiguchi0826@gmail.com>
2024-11-06 13:58:08 -0800

    adjust timeline and add height props

--- src/components/common/ApplicationStepMobile.tsx
+++ src/components/common/ApplicationStepMobile.tsx
@@ -9,6 +9,7 @@ export type Props = {
   steps: StepsType[];
   title: string;
   circleSpacing?: number;
+  height: string
 };
 
 /**
@@ -20,16 +21,16 @@ export type Props = {
  * <ApplicationStepMobile steps={Steps} title="Where to start?">
  * ```
  */
-export function ApplicationStepMobile({ steps, title, circleSpacing }: Props) {
+export function ApplicationStepMobile({ steps, title, circleSpacing, height }: Props) {
   return (
     <SectionContainer className="flex flex-col gap-4 p-4 xl:hidden">
       <h2 className="text-xl font-bold">{title}</h2>
-      <div className="flex items-center gap-4">
+      <div className="flex items-start gap-4">
         <TimelineMobile
           circleSpacing={circleSpacing}
           numCircles={steps.length}
         />
-        <ApplicationSteps steps={steps} />
+        <ApplicationSteps steps={steps} height={height} />
       </div>
       <div>
         <Button className="w-full">
