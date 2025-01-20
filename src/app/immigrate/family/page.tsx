860c97086e4693918b38e1a83a344da7e88fccce
SauloAC <saulo.andrade.costa@gmail.com>
2025-01-20 14:10:53 -0800

    Applying Spanish Translation to ImmigrationSteps

--- src/app/immigrate/family/page.tsx
+++ src/app/immigrate/family/page.tsx
@@ -2,11 +2,13 @@ import { ApplicationStepDesktop } from "@/components/common/ApplicationStepDeskt
 import { ApplicationStepMobile } from "@/components/common/ApplicationStepMobile";
 import { HeaderPicture } from "@/components/common/HeaderPicture";
 import familySP from "@/public/familySP.jpg";
+import { useTranslations } from "next-intl";
 import { EEPrograms } from "../_components/EEPrograms";
 import { IntroductionFamily } from "../_components/IntroductionFamily";
 import { EPSteps } from "../content/steps";
 
 export default function ExpressEntry() {
+  const t = useTranslations("ImmigrationEPSteps");
   return (
     <>
       <HeaderPicture
@@ -17,8 +19,13 @@ export default function ExpressEntry() {
 
       <IntroductionFamily />
 
-      <ApplicationStepDesktop steps={EPSteps} title="What is the process?" />
-      <ApplicationStepMobile steps={EPSteps} title="What is the process?" circleSpacing={110} height="h-[130px]" />
+      <ApplicationStepDesktop steps={EPSteps()} title={t("EPStepsHeading")} />
+      <ApplicationStepMobile
+        steps={EPSteps()}
+        title={t("EPStepsHeading")}
+        circleSpacing={110}
+        height="h-[130px]"
+      />
 
       <EEPrograms />
     </>
