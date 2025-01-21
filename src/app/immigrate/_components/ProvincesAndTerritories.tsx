124c399b2169b3096c4daa9558e6824d2afae6fa
SauloAC <saulo.andrade.costa@gmail.com>
2025-01-21 15:44:34 -0800

    Add Translations to PNP Page

--- src/app/immigrate/_components/ProvincesAndTerritories.tsx
+++ src/app/immigrate/_components/ProvincesAndTerritories.tsx
@@ -4,29 +4,24 @@ import { HeavySubHeader } from "@/components/common/text/HeavySubHeader";
 import { Paragraph } from "@/components/common/text/Paragraph";
 import { SubHeading } from "@/components/common/text/SubHeading";
 import Provinces from "@/public/Provinces-and-Territories.jpg";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { PNPPrograms } from "../content/pnpPrograms";
 
 export const ProvincesAndTerritories = () => {
+  const t = useTranslations("ProvincesAndTerritories");
   return (
     <>
       <SectionContainer className="mb-6 py-16">
         <SectionContainer>
-          <Heading>Provinces and Territories</Heading>
-          <Image
-            src={Provinces}
-            alt="Provincial Nominee Programs by Province"
-            width={1200}
-            height={400}
-          />
+          <Heading>{t("heading")}</Heading>
+          <Image src={Provinces} alt={t("heading")} width={1200} height={400} />
         </SectionContainer>
 
         <SectionContainer>
-          <SubHeading className="pt-10 pb-3">
-            Provincial Nominee Programs by Province
-          </SubHeading>
+          <SubHeading className="pt-10 pb-3">{t("subheading")}</SubHeading>
           <div className="bg-primary-red w-[50px] h-[2.5px] mb-8"></div>
-          {PNPPrograms.map((program) => (
+          {PNPPrograms().map((program) => (
             <div key={program.title}>
               <HeavySubHeader className="mb-6">{program.title}</HeavySubHeader>
               <Paragraph className="mb-6 text-justify">
