ec955da55384c6573d5be022c114e9d226e56154
YuiKonaka <youthfuldays585@gmail.com>
2025-01-22 10:17:59 -0800

    Modified ResponsiveImage

--- src/app/_home-components/quiz/QuizResult.tsx
+++ src/app/_home-components/quiz/QuizResult.tsx
@@ -1,49 +1,46 @@
-import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/QuizDialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import { BookFreeConsultation } from "@/components/common/BookFreeConsultation";
+import { ResponsiveImage } from "@/components/common/ResponsiveImage";
 import { Heading } from "@/components/common/text/Heading";
 import { HeavyBody } from "@/components/common/text/HeavyBody";
 import { SubHeading } from "@/components/common/text/SubHeading";
-import Image from "next/image";
+import {
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/QuizDialog";
 import expressEntry from "@/public/expressEntry.jpg";
+import { Cross2Icon } from "@radix-ui/react-icons";
 import { useTranslations } from "next-intl";
-import { BookFreeConsultation } from "@/components/common/BookFreeConsultation";
-import { ResponsiveImage } from "@/components/common/ResponsiveImage";
 
 type Props = {
-    onClose: () => void;
-}
+  onClose: () => void;
+};
 
 export default function QuizResult({ onClose }: Props) {
-    const t = useTranslations("Home");
+  const t = useTranslations("Home");
 
-    return (
-        <>
-            <DialogHeader>
-                <DialogTitle className="mt-5">
-                    <button onClick={onClose}>
-                        <Cross2Icon className="fixed right-5 top-5 h-7 w-7" />
-                    </button>
-                </DialogTitle>
-            </DialogHeader>
-            <div className="space-y-3">
-                <SubHeading>{t("pathwayOption")}</SubHeading>
-                <Heading>{t("expressEntry")}</Heading>
-                <HeavyBody>{t("pathwayDescription")}</HeavyBody>
-                <ResponsiveImage
-                    alt="expressEntry"
-                    src={expressEntry}
-                    divClassName="h-[220px] w-[400px]"
-                />
-            </div>
-            <DialogFooter className="sm:justify-start">
-                <BookFreeConsultation className="w-full" />
-            </DialogFooter>
-        </>
-    )
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle className="mt-5">
+          <button onClick={onClose}>
+            <Cross2Icon className="fixed right-5 top-5 h-7 w-7 " />
+          </button>
+        </DialogTitle>
+      </DialogHeader>
+      <div className="space-y-3">
+        <SubHeading className="">{t("pathwayOption")}</SubHeading>
+        <Heading>{t("expressEntry")}</Heading>
+        <HeavyBody>{t("pathwayDescription")}</HeavyBody>
+        <ResponsiveImage
+          alt="expressEntry"
+          src={expressEntry}
+          divClassName="h-[220px] w-full max-w-[400px]lg:w-full md:w-full sm:w-full"
+        />
+      </div>
+      <DialogFooter className="sm:justify-start">
+        <BookFreeConsultation className="w-full" />
+      </DialogFooter>
+    </>
+  );
 }
--- src/components/ui/QuizDialog.tsx
+++ src/components/ui/QuizDialog.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { Cross2Icon } from "@radix-ui/react-icons"
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Dialog = DialogPrimitive.Root
+const Dialog = DialogPrimitive.Root;
 
-const DialogTrigger = DialogPrimitive.Trigger
+const DialogTrigger = DialogPrimitive.Trigger;
 
-const DialogPortal = DialogPrimitive.Portal
+const DialogPortal = DialogPrimitive.Portal;
 
-const DialogClose = DialogPrimitive.Close
+const DialogClose = DialogPrimitive.Close;
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
@@ -26,8 +25,8 @@ const DialogOverlay = React.forwardRef<
     )}
     {...props}
   />
-))
-DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
+));
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
@@ -49,8 +48,8 @@ const DialogContent = React.forwardRef<
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
-))
-DialogContent.displayName = DialogPrimitive.Content.displayName
+));
+DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({
   className,
@@ -58,13 +57,13 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
+      "flex flex-col space-y-1.5 text-center sm:text-left w-full max-w-[400px] sm:max-w-[300px] xs:max-w-[250px] lg:max-w-[400px]",
       className
     )}
     {...props}
   />
-)
-DialogHeader.displayName = "DialogHeader"
+);
+DialogHeader.displayName = "DialogHeader";
 
 const DialogFooter = ({
   className,
@@ -77,8 +76,8 @@ const DialogFooter = ({
     )}
     {...props}
   />
-)
-DialogFooter.displayName = "DialogFooter"
+);
+DialogFooter.displayName = "DialogFooter";
 
 const DialogTitle = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Title>,
@@ -92,8 +91,8 @@ const DialogTitle = React.forwardRef<
     )}
     {...props}
   />
-))
-DialogTitle.displayName = DialogPrimitive.Title.displayName
+));
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
 
 const DialogDescription = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Description>,
@@ -104,18 +103,18 @@ const DialogDescription = React.forwardRef<
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-))
-DialogDescription.displayName = DialogPrimitive.Description.displayName
+));
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
 export {
   Dialog,
-  DialogPortal,
-  DialogOverlay,
-  DialogTrigger,
   DialogClose,
   DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
   DialogDescription,
-}
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+};
