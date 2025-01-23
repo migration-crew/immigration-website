d7b7ecb0dbfd80df52e5205877610dcfc0b41658
David Torrez <dtorrez1996@gmail.com>
2025-01-22 19:09:34 -0500

    Translate KnowBeforeEnrolling

--- src/app/study/_components/KnowBeforeEnrolling.tsx
+++ src/app/study/_components/KnowBeforeEnrolling.tsx
@@ -1,14 +1,15 @@
-import { SectionContainer } from "@/components/common/SectionContainer";
-import { Heading } from "@/components/common/text/Heading";
-import { StudyCardList } from "./StudyCardList";
+import { SectionContainer } from '@/components/common/SectionContainer';
+import { Heading } from '@/components/common/text/Heading';
+import { useTranslations } from 'next-intl';
+import { StudyCardList } from './StudyCardList';
 
 export function KnowBeforeEnrolling() {
-  return (
-    <SectionContainer className="flex flex-col gap-6">
-      <Heading className="">
-        All you have to know before enrolling to study in Canada
-      </Heading>
-      <StudyCardList />
-    </SectionContainer>
-  );
+    const t = useTranslations('Study');
+
+    return (
+        <SectionContainer className='flex flex-col gap-6'>
+            <Heading className=''>{t('section2Title')}</Heading>
+            <StudyCardList />
+        </SectionContainer>
+    );
 }
