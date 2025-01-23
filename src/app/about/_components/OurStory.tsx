526837e8eca7fb50c32d7a8954a112970b3449bf
SauloAC <saulo.andrade.costa@gmail.com>
2025-01-22 23:10:48 -0800

    Fix About Us video width

--- src/app/about/_components/OurStory.tsx
+++ src/app/about/_components/OurStory.tsx
@@ -1,26 +1,25 @@
 import { SectionContainer } from "@/components/common/SectionContainer";
 import { Heading } from "@/components/common/text/Heading";
 import { Paragraph } from "@/components/common/text/Paragraph";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export const OurStory = () => {
-
-  const t = useTranslations('AboutUs');
+  const t = useTranslations("AboutUs");
 
   return (
-      <SectionContainer className='py-10 xl:py-[100px]'>
-          <Heading>{t('section4Title')}</Heading>
-          <Paragraph className='pt-4 pb-3 xl:pb-5'>{t('section4P1')}</Paragraph>
-          <div className='rounded-xl w-[398px] h-[225px] xl:w-[1080px] xl:h-[610px] overflow-hidden'>
-              <video
-                  controls
-                  preload='none'
-                  poster='/ourStory.png'
-                  className='rounded-xl w-full h-full object-cover'
-              >
-                  <source src='/OurStory.mp4' type='video/mp4' />
-              </video>
-          </div>
-      </SectionContainer>
+    <SectionContainer className="py-10 xl:py-[100px]">
+      <Heading>{t("section4Title")}</Heading>
+      <Paragraph className="pt-4 pb-3 xl:pb-5">{t("section4P1")}</Paragraph>
+      <div className="rounded-xl w-full h-[225px] xl:w-[1080px] xl:h-[610px] overflow-hidden">
+        <video
+          controls
+          preload="none"
+          poster="/ourStory.png"
+          className="rounded-xl w-full h-full object-cover"
+        >
+          <source src="/OurStory.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </SectionContainer>
   );
 };
