8c8d971bc42622a4c8964e84003050f4274d85de
SauloAC <saulo.andrade.costa@gmail.com>
2025-02-07 01:18:59 -0800

    Fix Header Height

--- src/app/_header/Header.tsx
+++ src/app/_header/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
   const classNameForMobile = cn("flex lg:hidden");
 
   return (
-    <header className="px-6 py-2 bg-primary-white fixed z-50 top-0 w-full h-[72px]">
+    <header className="px-6 py-2 bg-primary-white fixed z-50 top-0 w-full h-[102px]">
       <div className="flex items-center justify-between">
         <LogoButton />
         {/* For Desktop */}
