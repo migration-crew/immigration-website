23e4f8ab02ea4de77d6b930a18bee2ca78cf6cee
SauloAC <saulo.andrade.costa@gmail.com>
2025-02-08 19:00:32 -0800

    add sheetTitle and SheetDescription to fix error in humberger menu

--- src/app/_header/_components/HamburgerMenu.tsx
+++ src/app/_header/_components/HamburgerMenu.tsx
@@ -1,39 +1,47 @@
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetFooter,
-    SheetTrigger,
-} from "@/components/ui/SheetForHeader"
-import { contactData } from '@/data/ContactData'
-import { Menu } from 'lucide-react'
-import ContactMenu from '../../../components/common/ContactMenu'
-import { HeaderComponent } from '../Header'
-import NavigationMenuForMobile from './NavigationMenuForMobile'
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/SheetForHeader";
+import { contactData } from "@/data/ContactData";
+import { Menu } from "lucide-react";
+import ContactMenu from "../../../components/common/ContactMenu";
+import { HeaderComponent } from "../Header";
+import NavigationMenuForMobile from "./NavigationMenuForMobile";
 
 type Props = {
-    className: string | undefined
-    immigrateComponents: HeaderComponent[]
-    workComponents: HeaderComponent[]
-}
+  className: string | undefined;
+  immigrateComponents: HeaderComponent[];
+  workComponents: HeaderComponent[];
+};
 
-export default function HamburgerMenu({className, immigrateComponents, workComponents}: Props) {
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                <Menu className={`"h-10 w-10" ${className}`} />
-            </SheetTrigger>
-            <SheetContent>
-                <NavigationMenuForMobile 
-                    immigrateComponents={immigrateComponents}
-                    workComponents={workComponents}
-                />
-                <SheetFooter>
-                    <SheetClose asChild>
-                        <ContactMenu className="fixed bottom-10" components={contactData}/>
-                    </SheetClose>
-                </SheetFooter>
-            </SheetContent>
-        </Sheet>
-    )
+export default function HamburgerMenu({
+  className,
+  immigrateComponents,
+  workComponents,
+}: Props) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Menu className={`"h-10 w-10" ${className}`} />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle />
+        <SheetDescription />
+        <NavigationMenuForMobile
+          immigrateComponents={immigrateComponents}
+          workComponents={workComponents}
+        />
+        <SheetFooter>
+          <SheetClose asChild>
+            <ContactMenu className="fixed bottom-10" components={contactData} />
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
 }
